717f7c122f055ca4b078bf57930b47b7e257575d
Nuur wehliye <wehliye10@icloud.com>
2024-09-09 18:42:04 +0200

    first commit

---
+++ .py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Tobias Fors", "email": "tobias.fors@yh.nackademin.se", "age": 30, "student_id": 11230, "grades": {"Python Programming 1": 1, "Database Technology": 4}},
+    {"name": "Karin Börjell", "email": "karin.borjell@yh.nackademin.se", "age": 32, "student_id": 11231, "grades": {"Python Programming 1": 1, "Python Programming 2": 3}},
+    {"name": "Daniel Eliasson", "email": "daniel.eliasson@yh.nackademin.se", "age": 29, "student_id": 11233, "grades": {"Python Programming 1": 1, "Business Skills": 2}},
+    {"name": "Magdalena Andersson", "email": "magdalena.andersson@yh.nackademin.se", "age": 50, "student_id": 11234, "grades": {"Python Programming 1": 1, "Web Frameworks in Python": 5}}
+]
+
